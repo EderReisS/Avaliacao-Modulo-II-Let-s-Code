ddcc2eb4ae3478a3b8f122b23584a9dfac00e4b0
EderReisS <ederdt.3@gmail.com>
2021-11-27 11:50:35 -0300

    atualização do requirements

--- api_credit.py
+++ api_credit.py
@@ -2,6 +2,8 @@
 from flask import Flask, request
 from flask_restful import Resource, Api
 import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
 
 
 app = Flask(__name__)
@@ -10,12 +12,43 @@ api = Api(app)
 url_data="https://raw.githubusercontent.com/EderReisS/Python_Machine_Learning/main/input_Files/Credit.csv"
 df_credit = pd.read_csv(url_data)
 
+class data_helper():
+    
+    @staticmethod
+    def agg_quantitative(data: pd.DataFrame, var: str = 'credit_amount') -> pd.DataFrame:
+        quali_var = 'job'
+        quanti_var = var
+                
+        df_agg =data.groupby(by=quali_var).agg(
+            {quanti_var:['mean','sum','std','median','count']
+            }
+        ).round(2)
+        
+        return df_agg
+    
+    @staticmethod
+    def graph_plot(data: pd.DataFrame, var: str = 'credit_amount') -> None:
+        graph = plt.figure(figsize=(15, 10))
+        sns.boxplot(data = data, x = 'job', y = var, hue = 'class')
+        plt.savefig('graph.jpeg')
+
+
 class credit_purpose(Resource):
     def get(self, purpose_):
-        purpose_filter= df_credit['purpose']== purpose_
-        credit_purpose = df_credit[purpose_filter]
-        print(credit_purpose.to_json(orient='columns'))
-        return credit_purpose.to_json(orient='columns')
+        purpose_filter = df_credit['purpose'] == purpose_
+        df_filtered = df_credit[purpose_filter]
+        
+        #persistir como csv e json a partir 
+        name_agg = 'credit_amount'
+        df_agg = data_helper.agg_quantitative(data = df_filtered, var = name_agg ) 
+        df_agg.to_json(f'{name_agg}.json', orient='columns')
+        df_agg.to_csv(f'{name_agg}.csv')
+        
+        #salvar o gráfico
+        data_helper.graph_plot(data = df_filtered)
+        
+        #retorno como API
+        return df_filtered.to_json(orient='columns')
     
 class purposes(Resource):
     def get(self):
